1491f6afc7d834f8ebe3c5ee151be55266879029
Yu-Wei Tseng <dadavidtseng@gmail.com>
2026-01-24 10:14:43 -0600

    chore: temporary push (migration from laptop to desktop)

--- Code/Game/Framework/GameCommon.hpp
+++ Code/Game/Framework/GameCommon.hpp
@@ -23,15 +23,6 @@ extern Game*                  g_game;
 extern Renderer*              g_renderer;
 extern RandomNumberGenerator* g_rng;
 
-//-----------------------------------------------------------------------------------------------
-// DebugRender-related
-//
-void DebugDrawRing(Vec2 const& center, float radius, float thickness, Rgba8 const& color);
-void DebugDrawLine(Vec2 const& start, Vec2 const& end, float thickness, Rgba8 const& color);
-void DebugDrawGlowCircle(Vec2 const& center, float radius, Rgba8 const& color, float glowIntensity);
-void DebugDrawGlowBox(Vec2 const& center, Vec2 const& dimensions, Rgba8 const& color, float glowIntensity);
-void DebugDrawBoxRing(Vec2 const& center, float radius, float thickness, Rgba8 const& color);
-
 //----------------------------------------------------------------------------------------------------
 template <typename T>
 void GAME_SAFE_RELEASE(T*& pointer)
